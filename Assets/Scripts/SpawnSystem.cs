b7eca873828c30475bfea5a730851242c4f8da34
Keijiro Takahashi <keijiro@gmail.com>
2023-07-02 23:27:55 +0900

    Use Entities.ForEach

--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -4,34 +4,55 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 
-[BurstCompile]
-public partial struct SpawnSystem : ISystem
+public partial class SpawnSystem : SystemBase
 {
-    [BurstCompile]
-    public void OnCreate(ref SystemState state)
-      => state.RequireForUpdate<Config>();
+    protected override void OnCreate()
+      => RequireForUpdate<Config>();
 
-    [BurstCompile(CompileSynchronously = true)]
-    public void OnUpdate(ref SystemState state)
+    protected override void OnUpdate()
     {
         var config = SystemAPI.GetSingleton<Config>();
-        var random = new Random(config.RandomSeed);
 
-        var instances = state.EntityManager.Instantiate
+        var instances = EntityManager.Instantiate
           (config.Prefab, config.SpawnCount, Allocator.Temp);
 
+        /*
+        var random = new Random(config.RandomSeed);
         foreach (var entity in instances)
         {
             ref var xform = ref SystemAPI.GetComponentRW<LocalTransform>(entity).ValueRW;
             ref var dancer = ref SystemAPI.GetComponentRW<Dancer>(entity).ValueRW;
             ref var walker = ref SystemAPI.GetComponentRW<Walker>(entity).ValueRW;
+
             xform.Position = random.NextOnDisk() * config.SpawnRadius;
             xform.Rotation = random.NextYRotation();
+
             dancer.Speed = random.NextFloat(1, 8);
+
             walker.ForwardSpeed = random.NextFloat(0.1f, 0.8f);
             walker.AngularSpeed = random.NextFloat(0.5f, 4);
         }
+        */
 
-        state.Enabled = false;
+        Entities.ForEach(
+            (int entityInQueryIndex,
+             ref LocalTransform xform,
+             ref Dancer dancer,
+             ref Walker walker) =>
+            {
+                var random = Random.CreateFromIndex
+                  (config.RandomSeed + (uint)entityInQueryIndex);
+
+                xform.Position = random.NextOnDisk() * config.SpawnRadius;
+                xform.Rotation = random.NextYRotation();
+
+                dancer.Speed = random.NextFloat(1, 8);
+
+                walker.ForwardSpeed = random.NextFloat(0.1f, 0.8f);
+                walker.AngularSpeed = random.NextFloat(0.5f, 4);
+            }
+        ).ScheduleParallel();
+
+        Enabled = false;
     }
 }
