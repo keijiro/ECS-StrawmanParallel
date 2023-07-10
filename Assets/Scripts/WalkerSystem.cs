33d323b28e2c76cd8894d56adb00f122599ca3b6
Keijiro Takahashi <keijiro@gmail.com>
2023-07-10 14:10:38 +0900

    System group

--- Assets/Scripts/WalkerSystem.cs
+++ Assets/Scripts/WalkerSystem.cs
@@ -3,6 +3,7 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 
+[UpdateBefore(typeof(TransformSystemGroup))]
 public partial struct WalkerSystem : ISystem
 {
     [BurstCompile]
